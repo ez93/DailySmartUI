45a04b37a262e9db6956647d6e93acf26d21d771
ez93 <chubbs_93@yahoo.com>
2019-07-19 22:25:24 -0700

    connected recent posts to redux

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import thunk from 'redux-thunk';
 
 import reducers from './reducers';
 
