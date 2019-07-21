41607fa59f633acd4cdc8e7658f1f9543e697efc
ez93 <chubbs_93@yahoo.com>
2019-07-20 19:33:06 -0700

    added mapState to Props and recent posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,17 @@
-import{SET_RECENT_POSTS} from './types';
+import { 
+    SET_RECENT_POSTS
+} from './types';
 
 import axios from 'axios';
 
-export function fetchRecentPosts(){
-    return function(dispatch){
+export function fetchRecentPosts() {
+    return function(dispatch) {
         axios.get('https://api.dailysmarty.com/posts')
-        .then(response => {
-            console.log(response.data.posts);
-            dispatch({
-                type: SET_RECENT_POSTS,
-                payload: response.data.posts
-
-        })
-    })
+            .then(response => {
+                dispatch({
+                    type: SET_RECENT_POSTS,
+                    payload: response.data.posts
+                })
+            })
     }
 }
--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,11 +1,28 @@
 import React, {Component} from 'react';
 
 import { connect } from 'react-redux';
+
 import * as actions from '../actions';
 
 class RecentPosts extends Component{
+    
     componentDidMount(){
         this.props.fetchRecentPosts();
+    }
+    renderPosts= function() {
+        const posts = this.props.recentPosts.map((post, index) => {
+            
+            if(index < 3){
+            return (
+                
+            <li key={index}>
+                {post.title}
+            </li>
+            )
+            }
+        })
+    
+        return posts
     }
     
     render() {
@@ -16,10 +33,8 @@ class RecentPosts extends Component{
                         Recent Posts
                     </div>
 
-                    <ul>
-                    <li>recent post 0</li>
-                    <li>recent post 1</li>
-                    <li>recent post 2</li>
+                    <ul className="recent-posts_posts">
+                    {this.renderPosts()}
                     </ul>
 
                 </div>
@@ -29,4 +44,10 @@ class RecentPosts extends Component{
     }
 }
 
-export default connect(null, actions)(RecentPosts);
+function mapStateToProps(state){
+    return{
+        recentPosts: state.posts.recentPosts
+    }
+}
+
+export default connect(mapStateToProps, actions)(RecentPosts);
