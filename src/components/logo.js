f7688dc8ea5ada3c6f33e5182b5308d56a73c935
ez93 <chubbs_93@yahoo.com>
2019-07-28 09:56:37 -0700

    modified post comoponent to render a result post and setup render links

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Logo extends Component{
     render() {
--- src/components/results.js
+++ src/components/results.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import Logo from './logo';
 import SearchBar from './SearchBar';
-import ResultsPosts from './resultsPost';
+import ResultsPosts from './resultsPosts';
 
 import { connect } from 'react-redux';
 import * as actions from '../actions';
